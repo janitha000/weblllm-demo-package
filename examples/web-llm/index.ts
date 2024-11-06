f939c25b61e6949ce15379565df02219c12acb88
Janitha Tennakoon <janitha@randomsoftware.net>
2024-11-06 18:24:58 +0530

    [ADD] Change model

--- examples/web-llm/index.ts
+++ examples/web-llm/index.ts
@@ -15,7 +15,8 @@ async function initEngine() {
     setLabel("init-label", report.text);
   };
   // Option 1: If we do not specify appConfig, we use `prebuiltAppConfig` defined in `config.ts`
-  const selectedModel = "Phi-3-mini-4k-instruct-q4f16_1-MLC-1k";
+  // const selectedModel = "Phi-3-mini-4k-instruct-q4f16_1-MLC-1k";
+  const selectedModel = "gemma-2-2b-it-q4f16_1-MLC-1k";
   engine = await webllm.CreateMLCEngine(
     selectedModel,
     {
@@ -37,8 +38,10 @@ async function initEngine() {
          <table border="0" cellpadding="0" cellspacing="0" style="caption-side:bottom;border-collapse:collapse;color:rgb(33,37,41);font-family:"Courier New"">
             <tbody style="box-sizing:border-box;border-style:solid;border-width:0px">
                <tr style="box-sizing:border-box;border-style:solid;border-width:0px">
-                  <td valign="top" style="box-sizing:border-box;border-color:inherit;border-style:solid;border-width:0px"><font size="2" face="Arial" style="box-sizing:border-box"><span style="box-sizing:border-box;font-weight:bolder"><u style="box-sizing:border-box">Claim:  </u></span></font></td>
-                  <td style="box-sizing:border-box;border-color:inherit;border-style:solid;border-width:0px"><font size="2" face="Arial" style="box-sizing:border-box"><span style="box-sizing:border-box;font-weight:bolder"><u style="box-sizing:border-box">13-76H2-57L01</u></span></font></td>
+                  <td valign="top" style="box-sizing:border-box;border-color:inherit;border-style:solid;border-width:0px"><font size="2" face="Arial" style="box-sizing:border-box"><span style="box-sizing:border-box;font-weight:bolder"><u style="box-sizing:border-box">First Name:  </u></span></font></td>
+                  <td style="box-sizing:border-box;border-color:inherit;border-style:solid;border-width:0px"><font size="2" face="Arial" style="box-sizing:border-box"><span style="box-sizing:border-box;font-weight:bolder"><u style="box-sizing:border-box">Janitha</u></span></font></td>
+                  <td valign="top" style="box-sizing:border-box;border-color:inherit;border-style:solid;border-width:0px"><font size="2" face="Arial" style="box-sizing:border-box"><span style="box-sizing:border-box;font-weight:bolder"><u style="box-sizing:border-box">Last Name:  </u></span></font></td>
+                  <td style="box-sizing:border-box;border-color:inherit;border-style:solid;border-width:0px"><font size="2" face="Arial" style="box-sizing:border-box"><span style="box-sizing:border-box;font-weight:bolder"><u style="box-sizing:border-box">Tennakoon</u></span></font></td>
                </tr>
             </tbody>
          </table>
@@ -54,7 +57,7 @@ async function initEngine() {
     {
       role: "system",
       content:
-        "Parse the html given and extract the text from it. Return the text using json format {claimId: string, claimText: string}",
+        "Parse the html given and extract the text from it. Return the text using json format {firstName: string, lastName: string}",
     },
     {
       role: "user",
@@ -62,13 +65,15 @@ async function initEngine() {
     },
   ];
 
+  const label = document.getElementById("generate-label");
+  label.innerText = "Generating...";
+
   const reply = await engine.chat.completions.create({
     messages: messages as webllm.ChatCompletionMessageParam[],
   });
   console.log(reply.choices[0].message);
   console.log(reply.usage);
 
-  const label = document.getElementById("generate-label");
   label.innerText = reply.choices[0].message.content;
 }
 
